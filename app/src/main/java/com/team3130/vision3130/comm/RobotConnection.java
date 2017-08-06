e37ea99a9030f1a6679e483b0523eb866c7b414f
Tantan4321 <kupa528@yahoo.com>
2017-08-06 13:36:17 -0500

    App port number changed

--- app/src/main/java/com/team3130/vision3130/comm/RobotConnection.java
+++ app/src/main/java/com/team3130/vision3130/comm/RobotConnection.java
@@ -21,7 +21,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class RobotConnection {
-    public static final int K_ROBOT_PORT = 8254;
+    public static final int K_ROBOT_PORT = 43130;
     public static final String K_ROBOT_PROXY_HOST = "localhost";
     public static final int K_CONNECTOR_SLEEP_MS = 100;
     public static final int K_THRESHOLD_HEARTBEAT = 800;
